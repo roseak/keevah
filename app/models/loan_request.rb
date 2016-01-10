a0f62a9a1d0ae168949322bab8da3c1b665071a1
roseak <rose.kohn@gmail.com>
2016-01-09 19:47:18 -0700

    first commit

--- app/models/loan_request.rb
+++ app/models/loan_request.rb
@@ -13,74 +13,101 @@ class LoanRequest < ActiveRecord::Base
   before_create :assign_default_image
 
   def assign_default_image
-    self.image_url = DefaultImages.random if self.image_url.to_s.empty?
+    Rails.cache.fetch("images-#{self.id}") do
+      self.image_url = DefaultImages.random if self.image_url.to_s.empty?
+    end
   end
 
   def owner
-    self.user.name
+    Rails.cache.fetch("owner-#{self.id}") do
+      self.user.name
+    end
   end
 
   def requested_by
-    self.requested_by_date.strftime("%B %d, %Y")
+    Rails.cache.fetch("request-by-#{self.id}") do
+      self.requested_by_date.strftime("%B %d, %Y")
+    end
   end
 
   def updated_formatted
-    self.updated_at.strftime("%B %d, %Y")
+    Rails.cache.fetch("updated-#{self.id}") do
+      self.updated_at.strftime("%B %d, %Y")
+    end
   end
 
   def repayment_begin
-    self.repayment_begin_date.strftime("%B %d, %Y")
+    Rails.cache.fetch("repayment-start-#{self.id}") do
+      self.repayment_begin_date.strftime("%B %d, %Y")
+    end
   end
 
   def funding_remaining
-    amount - contributed
+    Rails.cache.fetch("remaining-#{self.id}") do
+      amount - contributed
+    end
   end
 
   def self.projects_with_contributions
+    Rails.cache.fetch("projects-contributors-#{self.id}")
     where("contributed > ?", 0)
   end
 
   def list_project_contributors
-    project_contributors.map(&:name).to_sentence
+    Rails.cache.fetch("contributors-#{self.id}", expires_in: 1.hour) do
+      project_contributors.map(&:name).to_sentence
+    end
   end
 
   def progress_percentage
-    ((1.00 - (funding_remaining.to_f / amount.to_f)) * 100).to_i
+    Rails.cache.fetch("percent-#{self.id}") do
+      ((1.00 - (funding_remaining.to_f / amount.to_f)) * 100).to_i
+    end
   end
 
   def minimum_payment
-    if repayment_rate == "weekly"
-      (contributed - repayed) / 12
-    else
-      (contributed - repayed) / 3
+    Rails.cache.fetch("min-pay-#{self.id}") do
+      if repayment_rate == "weekly"
+        (contributed - repayed) / 12
+      else
+        (contributed - repayed) / 3
+      end
     end
   end
 
   def repayment_due_date
-    (repayment_begin_date + 12.weeks).strftime("%B %d, %Y")
+    Rails.cache.fetch("due-date-#{self.id}") do
+      (repayment_begin_date + 12.weeks).strftime("%B %d, %Y")
+    end
   end
 
   def pay!(amount, borrower)
-    repayment_percentage = (amount / contributed.to_f)
-    project_contributors.each do |lender|
-      repayment = lender.contributed_to(self).first.contribution * repayment_percentage
-      lender.increment!(:purse, repayment)
-      borrower.decrement!(:purse, repayment)
-      self.increment!(:repayed, repayment)
+    Rails.cache.fetch("pay-#{self.id}") do
+      repayment_percentage = (amount / contributed.to_f)
+      project_contributors.each do |lender|
+        repayment = lender.contributed_to(self).first.contribution * repayment_percentage
+        lender.increment!(:purse, repayment)
+        borrower.decrement!(:purse, repayment)
+        self.increment!(:repayed, repayment)
+      end
     end
   end
 
   def remaining_payments
-    (contributed - repayed) / minimum_payment
+    Rails.cache.fetch("remain-pay-#{self.id}") do
+      (contributed - repayed) / minimum_payment
+    end
   end
 
   def project_contributors
-    LoanRequestsContributor.where(loan_request_id: self.id).pluck(:user_id).map do |user_id|
-      User.find(user_id)
+    Rails.cache.fetch("loan_contributors-#{self.id}", expires_in: 1.hour) do
+      LoanRequestsContributor.where(loan_request_id: self.id).pluck(:user_id).map do |user_id|
+        User.find(user_id)
+      end
     end
   end
 
   def related_projects
-    (categories.flat_map(&:loan_requests) - [self]).shuffle.take(4)
+    LoanRequest.includes(:categories).where(categories: {id: self.categories[0].id}).order('RANDOM()').limit(4)
   end
 end
